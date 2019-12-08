ebc8e0edc7e5a148764241ee619cffc35eb803e7
Ori Refael <ori@funatix.club>
2019-12-08 15:32:33 +0200

    supporting all provided keys

--- android/src/main/java/com/eladcohen/capainstallref/CapacitorInstallReferrer.java
+++ android/src/main/java/com/eladcohen/capainstallref/CapacitorInstallReferrer.java
@@ -1,20 +1,38 @@
 package com.eladcohen.capainstallref;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 
+import java.util.Set;
+
 @NativePlugin()
 public class CapacitorInstallReferrer extends Plugin {
 
     @PluginMethod()
     public void echo(PluginCall call) {
-        String value = call.getString("value");
+
+        Bundle extras = intent.getExtras();
+        if (extras == null)
+            return;
 
         JSObject ret = new JSObject();
-        ret.put("value", value);
+
+        Set<String> keys = extras.keySet();
+
+        for (String key : keys) {
+            String value = extras.getString(key);
+            ret.put(key, value);
+
+        }
+
+
         call.success(ret);
     }
 }
--- android/src/main/java/com/eladcohen/capainstallref/Receiver.java
+++ android/src/main/java/com/eladcohen/capainstallref/Receiver.java
@@ -16,31 +16,30 @@ import android.util.Log;
 import android.content.SharedPreferences.Editor;
 import android.app.Activity;
 
+import java.util.Set;
+
 @NativePlugin()
 public class Receiver extends BroadcastReceiver {
-   
-   private static final String PREFS_NAME = "CapacitorStorage";
-   private static final String TAG = "InstallReceiver";
 
-        public void onReceive(Context context, Intent intent)
-        {
-            Log.e(TAG, "Context: " + context);
-            Bundle extras = intent.getExtras();
-            if (extras != null) {
-                String referrerString = extras.getString("referrer");
-                if (referrerString != null) {
-                    SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_NAME, Activity.MODE_PRIVATE);;
-                    Log.e(TAG, "Extras:");
-                    for (String keys : extras.keySet())
-                    {
-                        Log.e(TAG, keys + " -> " + extras.get(keys));
-                    }
-                    Editor edit = sharedPreferences.edit();
-                    edit.putString("referrer", referrerString);
-                    edit.commit();
-                }
-            }
+    private static final String PREFS_NAME = "CapacitorStorage";
+    private static final String TAG = "InstallReceiver";
 
+    public void onReceive(Context context, Intent intent) {
+        Log.e(TAG, "Context: " + context);
+        Bundle extras = intent.getExtras();
+        if (extras == null)
+            return;
 
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREFS_NAME, Activity.MODE_PRIVATE);
+        Editor edit = sharedPreferences.edit();
+
+        Set<String> keys = extras.keySet();
+
+        for (String key : keys) {
+            String value = extras.getString(key);
+            edit.putString(key, value);
         }
+
+        edit.commit();
+    }
 }
